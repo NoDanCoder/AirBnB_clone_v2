460c62e6925b83470ec2b51173969cba99ff6479
Alejandro Robayo <1249@holbertonschool.com>
2020-04-21 18:23:24 -0500

    [Add] web_flask - 03 task done

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Set up Flash Web Application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ return msg to GET request """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ return msg to GET request, path in /hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c(text):
+    """ return msg to GET request, path in /c/<var> """
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text="is cool"):
+    """ return msg to GET request, path in /python/<var> """
+    return "Python " + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    """ Entry point """
+    app.run(host='0.0.0.0', port=5000)
